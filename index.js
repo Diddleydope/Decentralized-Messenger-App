839d1c651a022cebdbe67568631b0aea5341e2b1
Samuel <samu.oneill@stud.unibas.ch>
2022-06-15 21:46:36 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
+
 src="https://cdn.jsdelivr.net/npm/gun/examples/jquery.js"
 src="https://cdn.jsdelivr.net/npm/gun/gun.js"
 src="https://cdn.jsdelivr.net/npm/gun/sea.js"
 src="https://cdn.jsdelivr.net/npm/gun/lib/webrtc.js"
 
-var gun = gun(['http://localhost:8765/gun', 'https://gun-manhattan.herokuapp.com/gun']);
+    var gun = Gun(['http://localhost:8765/gun', 'https://gun-manhattan.herokuapp.com/gun']);
+    var user = gun.user();
 
-var user = gun.user();
+    $('#up').on('click', function(e){
+      user.create($('#alias').val(), $('#pass').val());
+    });
 
-$('#up').on('click', function(e){
-  user.create($('#alias').val(), $('#pass').val());
-});
+    $('#sign').on('submit', function(e){
+      e.preventDefault();
+      user.auth($('#alias').val(), $('#pass').val());
+    });
 
-$('#sign').on('submit', function(e){
-  e.preventDefault();
-  user.auth($('#alias').val(), $('#pass').val());
-});
+    $('#said').on('submit', function(e){
+      e.preventDefault();
+      if(!user.is){ return }
+      user.get('said').set($('#say').val());
+      $('#say').val("");
+    });
 
-$('#said').on('submit', function(e){
-    e.preventDefault();
-    if(!user.is){ return }
-    user.get('said').set($('#say').val());
-    $('#say').val("");
-  });
+    function UI(say, id){
+      var li = $('#' + id).get(0) || $('<li>').attr('id', id).appendTo('ul');
+      $(li).text(say);
+    };
 
-function UI(say, id){
-    var li = $('#' + id).get(0) || $('<li>').attr('id', id).appendTo('ul');
-    $(li).text(say);
-  };
-
-  gun.on('auth', function(){
-    $('#sign').hide();
-    user.get('said').map().once(UI);
-  });
+    gun.on('auth', function(){
+      $('#sign').hide();
+      user.get('said').map().once(UI);
+    });
