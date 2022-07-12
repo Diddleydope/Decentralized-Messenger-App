d14f78c3814ba4b06df8f8ab1c13ca5ee21583e8
Samuel <samu.oneill@stud.unibas.ch>
2022-07-12 22:04:58 +0200

    first implementation of name displaying on UI

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-  
-  
-    var gun = Gun(['http://localhost:8765/gun', 'https://gun-manhattan.herokuapp.com/gun']);
-    var user = gun.user();
+var gun = Gun(['http://localhost:8765/gun', 'https://gun-manhattan.herokuapp.com/gun']);
+var user = gun.user();
 
-    $('#said').hide();
+$('#said').hide();
 
-    $('#up').on('click', function(e){
-      user.create($('#alias').val(), $('#pass').val());
-    });
+//user registration and login
+$('#up').on('click', function(e){
+  user.create($('#alias').val(), $('#pass').val());
+});
 
-    $('#sign').on('submit', function(e){
-      e.preventDefault();
-      user.auth($('#alias').val(), $('#pass').val());
-    });
+$('#sign').on('submit', function(e){
+  e.preventDefault();
+  user.auth($('#alias').val(), $('#pass').val());
+});
 
-    $('#said').on('submit', function(e){
-      e.preventDefault();
-      if(!user.is){return}
-      user.get('said').set($('#say').val());
-      $('#say').val("");
-    });
+//form submission
+$('#said').on('submit', function(e){
+  e.preventDefault();
+  //if(!user.is){return}
+  user.get('said').set($('#say').val());
+  $('#say').val("");
+});
 
-    function UI(say, id){
-      var li = $('#' + id).get(0) || $('<li>').attr('id', id).appendTo('ul');
-      $(li).text(say);
-    };
+//display on the UI
+function UI(say, id){
+  var li = $('#' + id).get(0) || $('<li>').attr('id', id).appendTo('ul');
+  var username = $('#alias').val();
+  $(li).text(username + ": " + say);
+};
 
-    gun.on('auth', function(){
-      $('#sign').hide();
-      $('#said').show();
-      user.get('said').map().once(UI);
-    });
+gun.on('auth', function(){
+  $('#sign').hide();
+  $('#said').show();
+  user.get('said').map().once(UI);
+});
