fa0f334b7ea6019a46fb139d05dee29018ef1e88
Samuel <samu.oneill@stud.unibas.ch>
2022-10-19 20:15:58 +0200

    attempt to set heroku as peer

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const GUN = require("gun");                     //npm i gun
 const { createServer } = require("http");
 
 const gun = GUN({
-    web: createServer().listen(8765),
+    web: createServer().listen(process.env.PORT || 5000),
 });
 gun.get("chat").map().once((data, id) => {
     console.log(data)
