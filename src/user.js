e45db89e816ab214f3c048608a5647faf2a850fc
Cedric Geissmann <cedric.geissmann@gmail.com>
2022-12-05 10:20:51 +0100

    added secure socket

--- src/user.js
+++ src/user.js
@@ -17,7 +17,7 @@ import { writable } from "svelte/store";
 // das kommt vlt in einer neueren Version wieder. Ansonsten einfach genügend
 // Relays zur Verfügung stellen.
 export const gun = GUN({
-    peers: ["http://172.105.92.43:8765/gun"]
+    peers: ["https://172.105.92.43:8765/gun"]
 });
 
 // Gun User
